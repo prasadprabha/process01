7c5e922c9f3195c61c7f310f3bfa28cfc06e67df
Prasad <prasadprabha07@gmail.com>
2021-05-16 20:05:30 +0530

    Latest changes

--- src/main/java/com/camunda/process/Process01Application.java
+++ src/main/java/com/camunda/process/Process01Application.java
@@ -1,9 +1,9 @@
-package org.camunda.bpm.process;
+package org.camunda.process;
 
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 
-@ProcessApplication("Process 01 App")
+@ProcessApplication("Process1")
 public class Process01Application extends ServletProcessApplication {
 
 }
